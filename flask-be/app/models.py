0840824d5d35f2489e8b187ad4b4f8484e0c28ee
Ade Indra Pratama <adeprathama@gmail.com>
2023-09-03 14:54:00 +0700

    Create db migrations on backend

---
+++ flask-be/app/models.py
@@ -0,0 +1,16 @@
+from app import db
+from datetime import datetime
+
+class Users(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    mobile_number = db.Column(db.String(255), index=True, nullable=False)
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255), nullable=False)
+    date_of_birth = db.Column(db.Date)
+    gender = db.Column(db.String(255))
+    email = db.Column(db.String(255), index=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<User {}>".format(self.name)
